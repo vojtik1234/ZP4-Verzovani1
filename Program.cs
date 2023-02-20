34d90397d27c726c039a2819f23638ce24e2b2d7
Vojtech Ludvik <vojtik1234@gmail.com>
2023-02-20 17:04:31 +0100

    upraveni writeline

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+moje nervy
+ */
+ 
+using System;
+
+namespace Verzovani
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Testik c. 11");
+			
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
